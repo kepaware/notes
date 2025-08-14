6413c54d43e49788cc998e4ba6073398e32aea13
KEPA <kepaware2160@gmail.com>
2025-08-15 10:17:30 +1200

    New Files

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,30 +1,33 @@
+import { AuthProvider } from "@/contexts/AuthContext";
 import { Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#ff8c00",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontSize: 20,
-          fontWeight: "bold",
-        },
-        contentStyle: {
-          paddingHorizontal: 10,
-          paddingTop: 10,
-          backgroundColor: "fff",
-        },
-      }}
-    >
-      {/* Main Page: */}
-      <Stack.Screen name="index" options={{ title: "Home" }} />
+    <AuthProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#ff8c00",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: "bold",
+          },
+          contentStyle: {
+            paddingHorizontal: 10,
+            paddingTop: 10,
+            backgroundColor: "fff",
+          },
+        }}
+      >
+        {/* Main Page: */}
+        <Stack.Screen name="index" options={{ title: "Home" }} />
 
-      {/* SubPages: */}
-      <Stack.Screen name="notes" options={{ headerTitle: "Notes" }} />
-    </Stack>
+        {/* SubPages: */}
+        <Stack.Screen name="notes" options={{ headerTitle: "Notes" }} />
+      </Stack>
+    </AuthProvider>
   );
 };
 
